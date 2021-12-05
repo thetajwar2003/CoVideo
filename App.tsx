fd5fbd434741f757c10a1d9e42f2b8f1f10079c9
thetajwar2003 <rahmantajwar131@gmail.com>
2021-12-05 15:17:34 -0500

    added amplify auth

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import awsExports from "./src/aws-exports";
+Amplify.configure(awsExports);
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -21,3 +25,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "covideo98fd452c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
